f2d05b63f07352258791b36a1402db7aa9143640
smzr <sammy@smzr.co.uk>
2023-07-28 10:47:03 +0100

    Replace wget with fs for image downloads

--- img-fetcher.js
+++ img-fetcher.js
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { execSync } = require('child_process');
 
 const [, , url, selector] = process.argv;
 
@@ -19,15 +18,46 @@ async function downloadImagesFromWebPage(url, selector) {
       if (src) images.push(src);
     });
 
-    const imageUrls = images.join('\n');
-    fs.writeFileSync('images.txt', imageUrls);
+    // Download images using fs
+    const downloadedSet = new Set();
+    let downloadedCount = 0;
+    for (const imageUrl of images) {
+      const imageName = imageUrl.split('/').pop();
 
-    // Using wget to download the images
-    execSync('wget -i images.txt', { stdio: 'inherit' });
+      if (!downloadedSet.has(imageName)) {
+        if (await downloadImage(imageUrl)) {
+          downloadedSet.add(imageName);
+          downloadedCount++;
+        }
+      }
+    }
 
-    console.log('Images downloaded successfully!');
+    if (downloadedCount > 0) {
+      console.log(`Successfully downloaded ${downloadedCount} image${downloadedCount > 1 ? 's' : ''}.`);
+    } else {
+      console.log('No images were downloaded.');
+    }
   } catch (error) {
     console.error('Error occurred:', error.message);
+  }
+}
+
+async function downloadImage(imageUrl) {
+  try {
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    const imageName = imageUrl.split('/').pop();
+
+    if (fs.existsSync(imageName)) {
+      console.log(`Skipping download: ${imageName} (Already downloaded)`);
+      return false;
+    }
+    
+    fs.writeFileSync(imageName, Buffer.from(response.data));
+    console.log(`Downloaded: ${imageName}`);
+    return true;
+  } catch (error) {
+    console.error('Error occurred while downloading image:', error.message);
+    return false;
   }
 }
 
